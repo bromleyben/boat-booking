c2d7c609d51272d13c743075ae87a38ed938431c
Ben Bromley <ben@inspiredsoftware.com.au>
2017-09-24 19:11:39 +1000

    Started Django Project

---
+++ boats/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.urls import reverse
+from django.views import generic
+
+from .models import Boat
+
+
+class IndexView(generic.ListView):
+    context_object_name = 'boat_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Boat.objects.order_by('name')
