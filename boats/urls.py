d664080eff7ea5057f9f7343ab725dee34268bc0
Ben Bromley <ben@inspiredsoftware.com.au>
2017-09-27 11:39:12 +1000

    t

--- boats/urls.py
+++ boats/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^[0-9]', views.DetailView.as_view(), name='detail'),
+    # url(r'^[0-9]', views.DetailView.as_view(), name='detail'),
 ]
