40f3be5e70e5eaf6ea891e0a56f37c551c23f6c2
dud <dimakharapaev@yandex.ru>
2017-09-27 14:00:59 +1000

    add classes

--- boats/models.py
+++ boats/models.py
@@ -3,6 +3,21 @@ from django.db import models
 
 class Boat(models.Model):
     name = models.CharField(max_length=200)
+    boatSize = models.CharField(max_length=200)
+    boatCapacity = models.IntegerField()
+    boatPrice = models.IntegerField()
 
-    def __str__(self):
-        return self.name
+class Booking(models.Model):
+    date = models.DateTimeField()
+    period = models.DurationField()
+    bookingNumber = models.IntegerField()
+
+
+
+class Payment(models.Model):
+    numberOfCard = models.IntegerField()
+    amount = models.IntegerField()
+    date = models.DateTimeField(max_length=20)
+    Transaction = models.ForeignKey(Booking)
+
+
